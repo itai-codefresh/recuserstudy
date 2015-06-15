fc4536e8b34b55022206dcd30a11fda61c10a798
Itai Gendler <itai@codefresh.io>
2015-06-15 13:05:06 +0300

    fix bgu username

--- server/api/movie/index.js
+++ server/api/movie/index.js
@@ -8,7 +8,7 @@ var router = express.Router();
 router.get('/withImages', controller.withImages);
 router.get('/getStats', controller.getStats);
 router.get('/getUsers', controller.getUsers);
-router.post('/deleteAll', controller.getUsers);
+router.post('/deleteAll', controller.deleteAll);
 router.post('/saveUserExperiment', controller.saveUserExperiment);
 
 module.exports = router;
